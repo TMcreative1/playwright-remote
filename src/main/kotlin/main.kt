938f333832c92f405f0a39e4aee1dafd29ada2fa
Tmcreative1 <tima.mamedov1@gmail.com>
2021-03-10 18:10:54 +0200

    update example of usage

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,11 @@
 import playwright.websocket.handler.MessageHandler
-import playwright.websocket.listener.WebSocketListener
 import playwright.websocket.provider.WebSocketProvider
-import java.time.Duration
 
 fun main(args: Array<String>) {
-    val webSocketListener = WebSocketListener()
-    val webSocketProvider = WebSocketProvider(webSocketListener)
+    val webSocketProvider = WebSocketProvider()
     webSocketProvider.connect("ws://127.0.0.1:4444/a3b02e1f28fdceb74ec3688eec70d626")
-    val messageHandler: MessageHandler = MessageHandler()
-    val message: String? = webSocketProvider.pollMessage(Duration.ofSeconds(5))
+    val messageHandler = MessageHandler()
+    val message: String? = webSocketProvider.pollMessage()
     messageHandler.processMessage(message!!)
     webSocketProvider.closeConnection()
 }
