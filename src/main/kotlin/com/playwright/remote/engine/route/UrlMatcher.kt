758a2f90cad401acfd60a2244beff7dc5e98428d
Tmcreative1 <tima.mamedov1@gmail.com>
2021-03-23 19:08:20 +0200

    change Unit to Boolean

--- src/main/kotlin/com/playwright/remote/engine/route/UrlMatcher.kt
+++ src/main/kotlin/com/playwright/remote/engine/route/UrlMatcher.kt
@@ -2,8 +2,7 @@ package com.playwright.remote.engine.route
 
 class UrlMatcher(
     private val rawSource: Any,
-    private val predicate: (String) -> Unit
+    private val predicate: (String) -> Boolean
 ) {
-
 
 }
