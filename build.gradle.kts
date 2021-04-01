ea9bff342cea9e0b3ee27d7ab1f70eb6928c1b84
Roman-Mitusov <roman.mitusov@gmail.com>
2021-04-01 19:00:39 +0300

    Upgrade playwright driver version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.nio.file.*
 import kotlin.collections.*
 
 val archs = listOf("mac", "linux", "win32", "win32_x64")
-val playwrightVersion = "1.10.0-next-1615230258000"
+val playwrightVersion = "1.11.0-next-1617087307000"
 
 plugins {
     kotlin("jvm") version "1.4.10"
