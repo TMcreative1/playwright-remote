b231a3be37699cb7b913856635d1092ddcfa79e0
Tmcreative1 <tima.mamedov1@gmail.com>
2021-04-19 17:56:57 +0300

    add jvmOverloads annotation

--- src/main/kotlin/com/playwright/remote/engine/options/HttpCredentials.kt
+++ src/main/kotlin/com/playwright/remote/engine/options/HttpCredentials.kt
@@ -2,7 +2,7 @@ package com.playwright.remote.engine.options
 
 import com.playwright.remote.engine.options.api.IBuilder
 
-data class HttpCredentials constructor(
+data class HttpCredentials @JvmOverloads constructor(
     var userName: String? = null,
     var password: String? = null,
     private val builder: IBuilder<HttpCredentials>
