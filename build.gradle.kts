fd79988ec4114a6508676945ac3e2a565582ac26
Tmcreative1 <tima.mamedov1@gmail.com>
2021-07-20 05:52:12 +0300

    reworked test logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.*
 import java.net.URL
@@ -30,8 +31,21 @@ dependencies {
 
 tasks.test {
     outputs.upToDateWhen { false }
-    testLogging.showStandardStreams = true
-    testLogging.exceptionFormat = FULL
+    testLogging {
+        events(FAILED, PASSED, SKIPPED, STANDARD_OUT)
+        exceptionFormat = FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+
+        debug {
+            events(STARTED, FAILED, PASSED, SKIPPED, STANDARD_ERROR, STANDARD_OUT)
+            exceptionFormat = FULL
+        }
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+    }
+
     useJUnitPlatform {
         include("**/Test*.class")
     }
