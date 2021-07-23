a4c06815edd0c2f0ed67ec2f6534bb5e2fe37151
TMcreative1 <32191555+TMcreative1@users.noreply.github.com>
2021-07-23 18:32:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("org.java-websocket:Java-WebSocket:1.5.2")
+    testImplementation("org.java-websocket:Java-WebSocket:1.5.2")
     implementation("com.google.code.gson:gson:2.8.6")
 }
 
