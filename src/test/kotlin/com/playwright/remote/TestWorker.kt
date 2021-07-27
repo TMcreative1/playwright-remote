d2ff919e7a8665ab98600a556ae8d050eb10c664
Tmcreative1 <tima.mamedov1@gmail.com>
2021-07-28 00:09:18 +0300

    fix

--- src/test/kotlin/com/playwright/remote/TestWorker.kt
+++ src/test/kotlin/com/playwright/remote/TestWorker.kt
@@ -199,7 +199,7 @@ class TestWorker : BaseTest() {
             page.evaluate("() => new Worker(URL.createObjectURL(new Blob(['console.log(1)'], {type: 'application/javascript'})))")
         }
         assertNotNull(worker)
-        val expectedVal = if (isFirefox()) "10,000.2" else "10\u00A0000,2"
+        val expectedVal = if (isFirefox()) "10000.2" else "10,000.2"
         assertEquals(expectedVal, worker.evaluate("() => (10000.20).toLocaleString()"))
         context.close()
     }
