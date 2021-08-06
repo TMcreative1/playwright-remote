d518ee519767dd2b0374acaa9d1b9fcdca726a9d
Tmcreative1 <tima.mamedov1@gmail.com>
2021-08-06 19:46:47 +0300

    add tests

--- src/test/kotlin/com/playwright/remote/TestDownload.kt
+++ src/test/kotlin/com/playwright/remote/TestDownload.kt
@@ -145,4 +145,30 @@ class TestDownload : BaseTest() {
         }
         pg.close()
     }
+
+    @Test
+    fun `check to report non navigation downloads`() {
+        httpServer.setRoute("/download") {
+            it.responseHeaders.add("Content-Type", "application/octet-stream")
+            it.sendResponseHeaders(200, 0)
+            OutputStreamWriter(it.responseBody).use { wr ->
+                wr.write("Hello world")
+            }
+        }
+
+        browser.newPage(NewPageOptions { it.acceptDownloads = true }).use {
+            it.navigate(httpServer.emptyPage)
+            it.setContent("<a download='file.txt' href='${httpServer.prefixWithDomain}/download'>download</a>")
+            val download = it.waitForDownload { it.click("a") }
+            assertNotNull(download)
+            assertEquals("file.txt", download.suggestedFilename())
+
+            val userPath = Files.createTempFile("download-", ".txt")
+            download.saveAs(userPath)
+            assertTrue(Files.exists(userPath))
+
+            val bytes = Files.readAllBytes(userPath)
+            assertEquals("Hello world", String(bytes, StandardCharsets.UTF_8))
+        }
+    }
 }
