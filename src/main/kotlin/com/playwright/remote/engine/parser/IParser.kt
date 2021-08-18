f756669d4e1c447ff0e282219927a4ca33d9a2e5
Tmcreative1 <tima.mamedov1@gmail.com>
2021-08-18 15:58:43 +0300

    add TestBrowserContextStorageState

--- src/main/kotlin/com/playwright/remote/engine/parser/IParser.kt
+++ src/main/kotlin/com/playwright/remote/engine/parser/IParser.kt
@@ -2,6 +2,7 @@ package com.playwright.remote.engine.parser
 
 import com.google.gson.JsonElement
 import com.playwright.remote.engine.serialize.CustomGson.Companion.gson
+import java.io.Reader
 
 interface IParser {
     companion object {
@@ -16,6 +17,10 @@ interface IParser {
         fun <T> fromJson(json: JsonElement, classOfT: Class<T>): T = gson().fromJson(json, classOfT)
 
         @JvmStatic
+        fun <T> fromJson(json: Reader, classOfT: Class<T>): T = gson().fromJson(json, classOfT)
+
+        @JvmStatic
         fun <T> convert(obj: Any?, classOfT: Class<T>): T = fromJson(toJson(obj), classOfT)
+
     }
 }
