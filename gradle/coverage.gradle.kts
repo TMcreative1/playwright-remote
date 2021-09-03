0a2a1be83bdfee3637181be10236e4a7902842d6
Tmcreative1 <tima.mamedov1@gmail.com>
2021-09-03 20:55:54 +0300

    rename packages

--- gradle/coverage.gradle.kts
+++ gradle/coverage.gradle.kts
@@ -1,8 +1,14 @@
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(false)
-        csv.required.set(false)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+configure<JacocoPluginExtension> {
+    toolVersion = "0.8.7"
+    reportsDirectory.set(layout.buildDirectory.dir("jacocoReportDir"))
+}
+
+tasks {
+    named<JacocoReport>("jacocoTestReport") {
+        reports {
+            xml.required.set(false)
+            csv.required.set(false)
+            html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        }
     }
-    dependsOn("test")
 }
