17195ff04ef346f6468d298f0caeec1a442b5862
Tmcreative1 <tima.mamedov1@gmail.com>
2021-09-17 16:55:50 +0300

    add TestPageNavigate

--- src/main/kotlin/io/github/tmcreative1/playwright/remote/core/enums/WaitUntilState.kt
+++ src/main/kotlin/io/github/tmcreative1/playwright/remote/core/enums/WaitUntilState.kt
@@ -1,7 +1,7 @@
 package io.github.tmcreative1.playwright.remote.core.enums
 
-enum class WaitUntilState {
-    LOAD,
-    DOMCONTENTLOADED,
-    NETWORKIDLE
+enum class WaitUntilState(val value: String) {
+    LOAD("load"),
+    DOMCONTENTLOADED("domcontentloaded"),
+    NETWORKIDLE("networkidle")
 }
---
+++ src/test/resources/scripts/history-api.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', () => {
+    history.pushState({}, '', '#1');
+});
